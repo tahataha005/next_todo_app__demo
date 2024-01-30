a3b5d86e6e09c56e6e492979bd93a9e007198f80
tahataha005 <taha.f.taha0@gmail.com>
2024-01-30 18:45:24 +0200

    Create <> Redux types

--- src/lib/redux/store.ts
+++ src/lib/redux/store.ts
@@ -1,11 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import AuthReducer from "./features/auth/authSlice";
+
 export const makeStore = () => {
   return configureStore({
-    reducer: {},
+    reducer: {
+      auth: AuthReducer,
+    },
   });
 };
 
 export type AppStore = ReturnType<typeof makeStore>;
-export type AppState = ReturnType<AppStore["getState"]>;
+export type RootState = ReturnType<AppStore["getState"]>;
 export type AppDispatch = AppStore["dispatch"];
