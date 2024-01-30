14cd4faaeabd00ef9676035e3d8be1a051dba41f
tahataha005 <taha.f.taha0@gmail.com>
2024-01-30 10:56:32 +0200

    Create <> Base layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "./styles/globals.css";
+import "./styles/utilities.css";
+import "./styles/colors.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="app">{children}</div>
+      </body>
     </html>
   );
 }
