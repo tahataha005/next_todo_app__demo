2cecb9c2c8b6f77bc3066e7c17e2d5ce286b883e
tahataha005 <taha.f.taha0@gmail.com>
2024-01-30 18:46:40 +0200

    Edit <> Using Logic hook for login form

--- src/app/components/forms/LoginForm.tsx
+++ src/app/components/forms/LoginForm.tsx
@@ -1,22 +1,37 @@
+import { useLogic } from "@/app/logic";
 import Button from "@/lib/ui/base/buttons/BaseButton";
 import Input from "@/lib/ui/base/inputs/BaseInput";
-import { redirect } from "next/navigation";
 import React from "react";
 
+export type Credentials = {
+  email: string;
+  password: string;
+};
+
 const LoginForm = () => {
+  const { handleChange, login } = useLogic();
+
   return (
     <div className="flex column center full-width">
-      <Input icon="email" label="Email" onChange={(e) => {}} />
+      <Input
+        icon="email"
+        label="Email"
+        onChange={(e) => {
+          handleChange("email", e.target.value);
+        }}
+      />
       <Input
         icon="password"
         type="password"
         label="Password"
-        onChange={(e) => {}}
+        onChange={(e) => {
+          handleChange("password", e.target.value);
+        }}
       />
       <Button
         text="Login"
-        onClick={() => {
-          redirect("/dashboard");
+        onClick={async () => {
+          await login();
         }}
       />
     </div>
