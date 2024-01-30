8a167a62b8719905b2383f6f0a5da36e1cebc570
tahataha005 <taha.f.taha0@gmail.com>
2024-01-30 19:28:24 +0200

    Fix <> Imports of buttons

--- src/app/components/forms/SignUpForm.tsx
+++ src/app/components/forms/SignUpForm.tsx
@@ -1,4 +1,4 @@
-import Button from "@/ui/base/buttons/BaseButton";
+import Button from "@/lib/ui/base/buttons/BaseButton";
 import Input from "@/lib/ui/base/inputs/BaseInput";
 import React, { FC } from "react";
 
