5d04eca5647663689771f41766c1ab61bcbe02ce
tahataha005 <taha.f.taha0@gmail.com>
2024-01-31 17:17:52 +0200

    Create <> Profile section

--- src/app/home/components/ProfileBar.tsx
+++ src/app/home/components/ProfileBar.tsx
@@ -14,6 +14,31 @@ const ProfileBar = () => {
       <div className={`flex center ${styles.profileBarHeader}`}>
         <h4 className="primary-text">TASK MANAGER</h4>
       </div>
+
+      <div className={`flex column center ${styles.profileBarInfo}`}>
+        <div className={`flex center circular ${styles.profilePhoto}`}>
+          <Image
+            src={profile}
+            alt="profile"
+            width={100}
+            height={100}
+            sizes="4"
+            priority
+          />
+        </div>
+        <div className={`flex column center ${styles.userInfo}`}>
+          <h3 className="bold black-text">Taha Taha</h3>
+          <p className="grey-text">taha.f.taha0@gmail.com</p>
+        </div>
+
+        <ProgressBar percentage={0.9} />
+
+        <div className="flex row center full-width">
+          <ProfileStat content="22" header="Done" />
+          <ProfileStat content="4" header="Left" />
+          <ProfileStat content="26" header="Total" />
+        </div>
+      </div>
     </div>
   );
 };
