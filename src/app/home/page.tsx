8987a61083396078dadd693099c6f29a4e747e19
tahataha005 <taha.f.taha0@gmail.com>
2024-02-04 11:30:15 +0200

    Create <> Tasks section in home content

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,7 +1,14 @@
 "use server";
 
+import Tasks from "./components/Tasks";
+import styles from "./home.module.css";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className={`flex row ${styles.homePage}`}>
+      <Tasks />
+    </div>
+  );
 };
 
 export default Home;
