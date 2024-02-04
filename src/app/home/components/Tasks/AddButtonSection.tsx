ee29e775888b2f3801a92e9db6c775fc2ea8ac4c
tahataha005 <taha.f.taha0@gmail.com>
2024-02-04 11:30:25 +0200

    Create <> Add task button

---
+++ src/app/home/components/Tasks/AddButtonSection.tsx
@@ -0,0 +1,11 @@
+const AddTask = () => {
+  return (
+    <div className="flex center full-width white-bg add-task">
+      <div className="flex center primary-bg white-text add-task-button">
+        Add
+      </div>
+    </div>
+  );
+};
+
+export default AddTask;
