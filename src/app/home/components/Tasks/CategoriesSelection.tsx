4fe14090d73ecf7234d69db74ff8aaaafafb41ff
tahataha005 <taha.f.taha0@gmail.com>
2024-02-04 11:31:03 +0200

    Create <> Categories filter

---
+++ src/app/home/components/Tasks/CategoriesSelection.tsx
@@ -0,0 +1,14 @@
+const Categories = () => {
+  return (
+    <div className="flex full-width white-bg categories-filter">
+      <div className="category-border"></div>
+      <select>
+        <option>Marketing</option>
+        <option>Development</option>
+        <option>Design</option>
+      </select>
+    </div>
+  );
+};
+
+export default Categories;
