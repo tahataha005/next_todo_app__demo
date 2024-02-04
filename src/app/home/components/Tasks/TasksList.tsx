52c0b999cd9c4956a81a6120e08494376139ab94
tahataha005 <taha.f.taha0@gmail.com>
2024-02-04 11:31:32 +0200

    Create <> Task list component

---
+++ src/app/home/components/Tasks/TasksList.tsx
@@ -0,0 +1,19 @@
+import Task from "./TaskTile";
+
+const TasksList = () => {
+  return (
+    <div className="flex column full-width white-bg tasks-list">
+      <Task />
+      <Task />
+      <Task />
+      <Task />
+      <Task />
+      <Task />
+      <Task />
+      <Task />
+      <Task />
+    </div>
+  );
+};
+
+export default TasksList;
